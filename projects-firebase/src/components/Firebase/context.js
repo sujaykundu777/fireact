ad458c3658dd2e7324e1351a868881edaef103ed
Sujay Kundu <sujaykundu777@gmail.com>
2019-04-22 00:19:44 +0530

    web app

--- projects-firebase/src/components/Firebase/context.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// creates a context using React Context API - Provider and Consumer 
-const FirebaseContext = React.createContext(null);
-
-export default FirebaseContext;
