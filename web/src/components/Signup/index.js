61caf196f36ffd27820d365a7b41c47f6a9ea5d9
Sujay Kundu <sujaykundu777@gmail.com>
2019-04-22 03:39:10 +0530

    check 3

--- web/src/components/Signup/index.js
+++ web/src/components/Signup/index.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Signup extends Component{
     render(){
         return(
-           <h1> Welcome User ! - Register Here</h1>
+           <h1> Register </h1>
 
            
         );
