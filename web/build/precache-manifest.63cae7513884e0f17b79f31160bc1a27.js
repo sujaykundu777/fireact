1460495e81429fe4cb2f64fa20caaf44a396411e
Sujay Kundu <sujaykundu777@gmail.com>
2019-04-22 03:00:19 +0530

    build v.0.1

---
+++ web/build/precache-manifest.63cae7513884e0f17b79f31160bc1a27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "53d675e73103542aecd2",
+    "url": "/static/js/main.aedc4392.chunk.js"
+  },
+  {
+    "revision": "fd09fdc1b9d5655abd87",
+    "url": "/static/js/2.5950bf30.chunk.js"
+  },
+  {
+    "revision": "53d675e73103542aecd2",
+    "url": "/static/css/main.0976dbc2.chunk.css"
+  },
+  {
+    "revision": "4c268c2ba30c8e28e5320dd769f9d030",
+    "url": "/index.html"
+  }
+];
